9723bce334965ae69319c2253361e30eb8ffe22c
ZionOyedokun <>
2019-01-10 08:44:52 -0600

    Chapter 01 - Chapter 04

---
+++ JavaProgramming/src/Chapter01/C1_1.java
@@ -0,0 +1,22 @@
+package Chapter01;
+
+/**
+ * Program to print welcome to java
+ *
+ * @author Zion Oyedokun
+ */
+public class C1_1 {
+
+    /**
+     * Main method
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+    }
+}
---
+++ JavaProgramming/src/Chapter01/P1.java
@@ -0,0 +1,18 @@
+package Chapter01;
+
+/**
+ * print welcome to java
+ *
+ * @author Zion Oyedokun
+ */
+public class P1 {
+
+    /**
+     * Main Method
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+    }
+}
